bdd2b2eaede70f7c8dc39ce998142abbeb84037e
Divyanshu Kumar <93030810+Divyanshu11011@users.noreply.github.com>
2024-06-14 20:44:38 +0530

    Add files via upload

---
+++ token-swaps/pages/_app.tsx
@@ -0,0 +1,40 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { WagmiConfig, createConfig } from "wagmi";
+import {
+  ConnectKitProvider,
+  ConnectKitButton,
+  getDefaultConfig,
+} from "connectkit";
+import { useEffect, useState } from "react";
+
+const config = createConfig(
+  getDefaultConfig({
+    // Required API Keys
+    alchemyId: "MzUaa0A87yexjd8UKcHm8HIr1f4aghxT",
+    walletConnectProjectId: "a8024e8262cb4e7102941a3577b5a5c0",
+
+    // Required
+    appName: "CRYPTOPULSE",
+  })
+);
+
+export default function App({ Component, pageProps }: AppProps) {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+
+  return (
+    <div
+      style={{
+        padding: "20px",
+      }}
+    >
+      <WagmiConfig config={config}>
+        <ConnectKitProvider>
+          <ConnectKitButton />
+          {mounted && <Component {...pageProps} />}
+        </ConnectKitProvider>
+      </WagmiConfig>
+    </div>
+  );
+}
